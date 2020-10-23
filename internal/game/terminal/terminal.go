2b063cf675868df92df1ea98adec152a18ce118a
J Gregory <john@johngregory.me.uk>
2020-10-23 17:28:18 +0100

    Fix #12 Windows OS screen clearing (#13)

---
+++ internal/game/terminal/terminal.go
@@ -0,0 +1,15 @@
+// +build !windows
+
+package terminal
+
+import (
+	"os"
+	"os/exec"
+)
+
+// Clear clears the terminal
+func Clear() {
+	cmd := exec.Command("clear")
+	cmd.Stdout = os.Stdout
+	cmd.Run()
+}
